e3aee52328136e69b405eaa55c03edbf4d2514fa
baileicheng <xbfighting@hotmail.com>
2017-12-29 17:39:55 +0800

    [mod] 1st commit

---
+++ c_1/chapter_1_1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_1
+{
+    public class chapter_1_1
+    {
+        /// <summary>
+        /// Bubble
+        /// </summary>
+        /// <param name="unSortedArray"></param>
+        /// <returns></returns>
+        public int[] Bubble(int[] unSortedArray)
+        {
+            int[] sortedArray = {};
+            if (unSortedArray != null && unSortedArray.Length > 0)
+            {
+                var length = unSortedArray.Length;
+                for (int i = 0; i < length; i++)
+                {
+                    for (int j = i + 1; j < length; j++)
+                    {
+                        if (unSortedArray[j] < unSortedArray[i])
+                        {
+                            int temp = unSortedArray[j];
+                            unSortedArray[j] = unSortedArray[i];
+                            unSortedArray[i] = temp;
+                        }
+                    }
+                }
+                sortedArray = unSortedArray;
+            }
+
+            return sortedArray;
+        }
+
+        /// <summary>
+        /// InsertSort
+        /// </summary>
+        /// <param name="unSortedArray"></param>
+        /// <returns></returns>
+        public List<int> InsertSort(List<int> unSortedArray)
+        {
+            var sortedArray = new List<int> {};
+            if (unSortedArray != null && unSortedArray.Count > 0)
+            {
+                var length = unSortedArray.Count;
+                for (var i = 0; i < length; i++)
+                {
+                    if (sortedArray.Count == 0)
+                    {
+                        sortedArray.Add(unSortedArray[i]);
+                    }
+                    else
+                    {
+                        for (var j = sortedArray.Count - 1; j >= 0; j--)
+                        {
+                            if (unSortedArray[i] > sortedArray[j])
+                            {
+                                if (j == sortedArray.Count - 1)
+                                    sortedArray.Add(unSortedArray[i]);
+                                else
+                                    sortedArray.Insert(j + 1, unSortedArray[i]);
+                                break;
+                            }
+                            if (j == 0)
+                            {
+                                sortedArray.Insert(j, unSortedArray[i]);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return sortedArray;
+        }
+
+
+        public int[] InsertSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int temp = array[i];
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (array[j] > temp)
+                    {
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                    else
+                        break;
+                }
+            }
+            return array;
+        }
+    }
+}
---
+++ c_1Tests/chapter_1_1Tests.cs
@@ -0,0 +1,151 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using c_1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace c_1.Tests
+{
+    [TestFixture]
+    public class chapter_1_1Tests
+    {
+        private chapter_1_1 _chapter1;
+        [SetUp]
+        public void SetUp()
+        {
+            _chapter1 = new chapter_1_1();
+        }
+
+        [Test]
+        public void Test_NormalArray_Bubble()
+        {
+            // Given
+            var expectedArray = new[] { 1, 2, 3, 4, 5, 6, };
+            var unSortedArray = new[] { 4, 5, 6, 1, 2, 3, };
+
+            // When
+            var sortedArray = _chapter1.Bubble(unSortedArray);
+
+            // Then
+            Assert.AreEqual(expectedArray, sortedArray);
+        }
+
+        [Test]
+        public void Test_Normal2Array_Bubble()
+        {
+            // Given
+            var expectedArray = new[] { 1, 2, 3, 4, 5, 6, };
+            var unSortedArray = new[] { 6, 5, 4, 3, 2, 1, };
+
+            // When
+            var sortedArray = _chapter1.Bubble(unSortedArray);
+
+            // Then
+            Assert.AreEqual(expectedArray, sortedArray);
+        }
+
+        [Test]
+        public void Test_SingleElementArray_Bubble()
+        {
+            // Given
+            var expectedArray = new[] { 6, };
+            var unSortedArray = new[] { 6, };
+
+            // When
+            var sortedArray = _chapter1.Bubble(unSortedArray);
+
+            // Then
+            Assert.AreEqual(expectedArray, sortedArray);
+        }
+
+        [Test]
+        public void Test_NoneArray_Bubble()
+        {
+            // Given
+            int[] expectedArray = { };
+            int[] unSortedArray = { };
+
+            // When
+            var sortedArray = _chapter1.Bubble(unSortedArray);
+
+            // Then
+            Assert.AreEqual(expectedArray, sortedArray);
+        }
+
+
+        [Test]
+        public void Test_NormalArray_InsertSort()
+        {
+            // Given
+            var expectedArray = new List<int> { 1, 2, 3, 4, 5, 6, };
+            var unSortedArray = new List<int> { 4, 5, 6, 1, 2, 3, };
+
+            // When
+            var sortedArray = _chapter1.InsertSort(unSortedArray);
+
+            // Then
+            Assert.AreEqual(expectedArray, sortedArray);
+        }
+
+        [Test]
+        public void Test_Normal2Array_InsertSort()
+        {
+            // Given
+            var expectedArray = new List<int> { 1, 2, 3, 4, 5, 6, };
+            var unSortedArray = new List<int> { 6, 5, 4, 3, 2, 1, };
+
+            // When
+            var sortedArray = _chapter1.InsertSort(unSortedArray);
+
+            // Then
+            Assert.AreEqual(expectedArray, sortedArray);
+        }
+
+        [Test]
+        public void Test_SingleElementArray_InsertSort()
+        {
+            // Given
+            var expectedArray = new List<int> { 6, };
+            var unSortedArray = new List<int> { 6, };
+
+            // When
+            var sortedArray = _chapter1.InsertSort(unSortedArray);
+
+            // Then
+            Assert.AreEqual(expectedArray, sortedArray);
+        }
+
+        [Test]
+        public void Test_NoneArray_InsertSort()
+        {
+            // Given
+            var expectedArray = new List<int> { };
+            var unSortedArray = new List<int> { };
+
+            // When
+            var sortedArray = _chapter1.InsertSort(unSortedArray);
+
+            // Then
+            Assert.AreEqual(expectedArray, sortedArray);
+        }
+
+
+
+        [Test]
+        public void Test_NormalArray_InsertSortA()
+        {
+            // Given
+            int[] expectedArray = { 1, 2, 3, 4, 5, 6, };
+            int[] unSortedArray = { 4, 5, 6, 1, 2, 3, };
+
+            // When
+            var sortedArray = _chapter1.InsertSort(unSortedArray);
+
+            // Then
+            Assert.AreEqual(expectedArray, sortedArray);
+        }
+    }
+}
