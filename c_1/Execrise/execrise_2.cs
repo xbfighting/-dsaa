aea3f1fcd52c8f46c1c689a13eb36c9128dce697
baileicheng <xbfighting@hotmail.com>
2018-01-03 17:27:23 +0800

    [add] words secret game part1

---
+++ c_1/Execrise/execrise_2.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_1.Execrise
+{
+    /// <summary>
+    /// 字谜游戏
+    /// </summary>
+    public class execrise_2
+    {
+        public string[] Dictionary { get; set; }
+
+        public string[,] CheckBoard { get; set; }
+
+        public static int[] WordLength = {3, 4, 5};
+
+        public void InitDiction(string[] dict)
+        {
+            this.Dictionary = dict;
+        }
+
+        public void InitCheckBoard(string[,] cb)
+        {
+            this.CheckBoard = cb;
+        }
+
+        public List<string> FindLetterCombination()
+        {
+            List<string> result = new List<string>();
+            int cloumn = this.CheckBoard.GetLength(0);
+            int row = this.CheckBoard.GetLength(1);
+            int directionCount = 6; // 寻词方向 6个
+
+            for (int i = 0; i < cloumn; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    for (int k = 0; k < directionCount; k++)
+                    {
+                        for (int l = 0; l < WordLength.Length; l++)
+                        {
+                            int wordLengthLimit = WordLength[l];
+
+                            switch (k)
+                            {
+                                case (int)DirectionEnum.Up:
+                                    if (j >= wordLengthLimit-1)
+                                    {
+                                        string word = "";
+                                        for (int m = 0; m < wordLengthLimit; m++)
+                                        {
+                                            word += CheckBoard[i, j - m];
+                                        }
+                                        result.Add(word);
+                                    }
+                                    break;
+                                case (int)DirectionEnum.Down:
+                                    if (j >= wordLengthLimit - 1)
+                                    {
+                                        string word = "";
+                                        for (int m = 0; m < wordLengthLimit; m++)
+                                        {
+                                            word += CheckBoard[i, j + m];
+                                        }
+                                        result.Add(word);
+                                    }
+                                    break;
+                                case (int)DirectionEnum.LeftDown:
+                                    break;
+                                case (int)DirectionEnum.RightDown:
+                                    break;
+                                case (int)DirectionEnum.LeftUp:
+                                    break;
+                                case (int)DirectionEnum.RightUp:
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+            
+
+            return result;
+        }
+
+        //private string[] FindLetterCombination
+    }
+
+    /// <summary>
+    /// 寻词方向
+    /// </summary>
+
+    public enum DirectionEnum : int
+    {
+        Up = 0,
+        Down = 1,
+        LeftDown = 2,
+        RightDown = 3,
+        LeftUp = 4,
+        RightUp = 5,
+    }
+}
